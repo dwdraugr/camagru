b363f0fe9365a00592b91edbfad9f6fc1b9ee9f0
Dwarven Shpere Draugr Deathlord <kostya.marinenkov@gmail.com>
2019-06-29 15:23:09 +0300

    Modify template, add some button

--- app/models/model_auth.php
+++ app/models/model_auth.php
@@ -4,11 +4,12 @@ class Model_Auth extends Model
     public function get_data($login, $password)
     {
         include "config/database.php";
+        $hash_passwd = hash("whirlpool", $password);
         $pdo = new PDO($dsn, $db_user, $db_pass, $opt);
         $pdo->exec("USE $db");
-        $sql = "SELECT * FROM users WHERE nickname = :nickname AND password = :password";
+        $sql = "SELECT * FROM users WHERE nickname = ? AND password = ?";
         $stmt = $pdo->prepare($sql);
-        if ($stmt->execute(array($login, $password))) {
+        if ($stmt->execute(array($login, $hash_passwd))) {
             $data = $stmt->fetchAll();
             return $data;
         }
--- app/views/template_view.php
+++ app/views/template_view.php
@@ -13,15 +13,20 @@
     <span>Camagru</span>
     <nav>
         <a href="/main/feed"><div class="navi">Feed</div></a>
-        <a href="/main/profile"><div class="navi">Profile</div></a>
         <a href="#add"><div class="navi">Add</div></a>
         <?php
         if (!isset($_SESSION['nickname']) and !isset($_SESSION['password']))
-            echo "<a href='/auth'><div class='navi'>Sign In</div></a>";
-        else
+            {
+                echo "<a href='/auth'><div class='navi'>Sign In</div></a>";
+                echo "<a href='/signup'><div class='navi'>Sign Up</div></a>";
+            }
+        else {
+            echo "<a href='/main/profile'><div id='user' class='navi'>{$_SESSION['nickname']}</div></a>";
+            echo "<a href='/settings'><div class='navi'>Settings</div></a>";
             echo "<a href='/auth/signout'><div class='navi'>Sign Out</div></a>";
+        }
         ?>
-    </nav>
+<!--    </nav>-->
 </header>
 <br>
 <main>
