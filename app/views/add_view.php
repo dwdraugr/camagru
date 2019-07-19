63787cae5aecf51e35ee13ebcc2d65e071b27373
Dwarven Shpere Draugr Deathlord <kostya.marinenkov@gmail.com>
2019-07-19 17:39:00 +0300

    Canvas is works

--- app/views/add_view.php
+++ app/views/add_view.php
@@ -48,19 +48,19 @@ SUC;
 <div id="create_post"">
 	<div id="image_field">
 	<form id="upload_form" enctype="multipart/form-data" action="/add/create/" method="post">
-	<input type="file" id="file_up" name="image_upload" accept="image/jpeg, image/png, image/gif" required="required">
+	<input type="file" onchange="readURL();" id="file_up" name="image_upload" accept="image/jpeg, image/png, image/gif" required="required">
 	<video id="video" width="640" height="480" autoplay></video>
-	<button id="snap">Snap Photo</button>
 	<canvas id="canvas" width="640" height="480"></canvas>
+	<img id="aaaa" src="" alt="preview"/>
 </form>
 </div>
-	<div id="side_menu">	
-		<div  id="sticker_bar">STICKER BAR</div>
+	<div id="side_menu">
+		<div id="sticker_bar"></div>
 		<div id ="description" style="text-align: center">Description<br><input type="text" form="upload_form" maxlength="250"
 																				required="required" name="description"></div>
 		<input id="submit" type="submit" form="upload_form">
 <!--		<div id="submit"><input type="submit" form="upload_form"></div></div>-->
-<button id="biba">BIBA</button>
+<button id="biba">START VIDEO</button>
 </div>
 </div>
 MAIN;
--- js/camera.js
+++ js/camera.js
@@ -1,23 +1,103 @@
-window.onload = function () {
+var canvas, context;
+var star_img = new Image();
+var img = new Image();
+var isDraggable = false;
+var isVideo = false;
+
+var currentX = 0;
+var currentY = 0;
+
+window.onload = function() {
+    document.getElementById('biba').addEventListener('click', start_camera);
+    document.getElementById('file_up')
+        .addEventListener('change', (e) => edit_photo(e.target.files));
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+
+    currentX = canvas.width/2;
+    currentY = canvas.height/2;
+
+    star_img.src = '/images/ricardo.png';
+};
+
+function start_camera() {
     var video = document.getElementById('video');
     if (navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
         video.srcObject = stream;
         video.play();
+        isVideo = true;
     }));
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    var video = document.getElementById('video');
-    document.getElementById("snap").addEventListener('click', function () {
-        context.drawImage(video, 0, 0, 640, 480);
-    })
-    document.getElementById('biba').addEventListener('click', function () {
-        var dataURL = context.toDataURL;
-        var formData = new FromData();
-        formData.append('image_upload', dataURL);
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/add/create', false);
-        xhr.setRequestHeader('Content-Type', false)
-        xhr.send(dataURL);
-    })
-};
+    _Go();
+}
 
+function edit_photo(img) {
+    _Go();
+}
+
+function _Go() {
+    _MouseEvents();
+    if (isVideo) {
+        setInterval(function () {
+            _ResetCanvas();
+            _DrawImage();
+        }, 1000 / 60);
+    }
+    else
+    {
+        setInterval(function () {
+            _ResetCanvas();
+            context.fillRect(0, 0, 640, 480);
+            context.drawImage(img, 0, 0, 640, 480);
+            _DrawImage();
+        }, 1000 / 60);
+    }
+}
+
+
+function _ResetCanvas() {
+    context.clearRect(0,0, canvas.width, canvas.height);
+}
+function _MouseEvents() {
+    canvas.onmousedown = function(e) {
+
+        var mouseX = e.pageX - this.offsetLeft;
+        var mouseY = e.pageY - this.offsetTop;
+
+
+        if (mouseX >= (currentX - star_img.width/2) &&
+            mouseX <= (currentX + star_img.width/2) &&
+            mouseY >= (currentY - star_img.height/2) &&
+            mouseY <= (currentY + star_img.height/2)) {
+            isDraggable = true;
+            currentX = mouseX;
+            currentY = mouseY;
+        }
+    };
+    canvas.onmousemove = function(e) {
+
+        if (isDraggable) {
+            currentX = e.pageX - this.offsetLeft;
+            currentY = e.pageY - this.offsetTop;
+        }
+    };
+    canvas.onmouseup = function(e) {
+        isDraggable = false;
+    };
+    canvas.onmouseout = function(e) {
+        isDraggable = false;
+    };
+}
+function _DrawImage() {
+    context.drawImage(star_img, currentX-(star_img.width/2), currentY-(star_img.height/2));
+}
+
+function readURL(input) {
+    img = new Image;
+    let file = document.querySelector('input[type=file]').files[0];
+    let reader = new FileReader();
+    reader.addEventListener('load', function () {
+        img.src = reader.result;
+    });
+    if (file)
+        reader.readAsDataURL(file);
+}
